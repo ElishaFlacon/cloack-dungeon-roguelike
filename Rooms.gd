a573d825c62422c5a7339d1e529aa3d667a6d591
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-05-04 23:09:53 +0500

    mini test new generation

--- Rooms.gd
+++ Rooms.gd
@@ -44,12 +44,18 @@ func _spawn_rooms() -> void:
 			var previous_room_tilemap: TileMap = previous_room.get_node("TileMap")
 			var previous_room_door: StaticBody2D = previous_room.get_node("Doors/Door")
 			var exit_tile_pos: Vector2 = previous_room_tilemap.world_to_map(previous_room_door.position)+ Vector2.UP * 2
-			var corridor_height: int = randi() % 6 + 2
+			var corridor_height: int = randi() % 6 + 8
 			for y in corridor_height:
 				previous_room_tilemap.set_cellv(exit_tile_pos + Vector2(-2, -y), LEFT_WALL_TILE_INDEX)
 				previous_room_tilemap.set_cellv(exit_tile_pos + Vector2(-1, -y), FLOOR_TILE_INDEX)
 				previous_room_tilemap.set_cellv(exit_tile_pos + Vector2(0, -y), FLOOR_TILE_INDEX)
 				previous_room_tilemap.set_cellv(exit_tile_pos + Vector2(1, -y), RIGHT_WALL_TILE_INDEX)
+				
+				previous_room_tilemap.set_cellv(exit_tile_pos + Vector2(1, -int(corridor_height / 2)), FLOOR_TILE_INDEX)
+				previous_room_tilemap.set_cellv(exit_tile_pos + Vector2(1, -int(corridor_height / 2) -1), FLOOR_TILE_INDEX)
+				previous_room_tilemap.set_cellv(exit_tile_pos + Vector2(y, -int(corridor_height / 2)), FLOOR_TILE_INDEX)
+				previous_room_tilemap.set_cellv(exit_tile_pos + Vector2(y, -int(corridor_height / 2) - 1), FLOOR_TILE_INDEX)
+				
 			var room_tilemap: TileMap = room.get_node("TileMap")
 			room.position = previous_room_door.global_position + Vector2.UP * room_tilemap.get_used_rect().size.y * TILE_SIZE + Vector2.UP * (1 + corridor_height) * TILE_SIZE + Vector2.LEFT * room_tilemap.world_to_map(room.get_node("Entrance/Position2D2").position).x * TILE_SIZE
 		add_child(room)
